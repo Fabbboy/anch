cc948fe86ac75832c1b8161fd609bcc8ea3202dc
Fabrice Schaub <fabrice.schaub@swisscom.com>
2023-12-28 11:44:30 +0100

    feat: advanced testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include "lexer/inc/lexer.h"
+#include <fstream>
 
+#define TESTING 0
+
+#define READ_CONTENTS(FILE_PATH) std::ifstream file(FILE_PATH); \
+  std::string contents((std::istreambuf_iterator<char>(file)), \
+  std::istreambuf_iterator<char>()); \
+  file.close();
+
+#ifdef TESTING
+static std::string tests[] = {
+  "tests/simple/num_n_op.anch"
+};
 int main() {
-  std::string input = R"(21*23.4 + 0x03 - 2 / 3.0 % .3)";
-  char* input_c = (char*)input.c_str();
-  Token** tokens = lex(input_c);
+  for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+    std::cout << "==== Testing: " << tests[i] << " ====" << std::endl;
+    READ_CONTENTS(tests[i]);
+    if (contents.empty()) {
+      std::cout << "File is empty" << std::endl;
+      return 1;
+    }
+    char* input_c = (char*)contents.c_str();
+    Token** tokens = lex(input_c);
 
-  for (int i = 0; i < token_count; i++) {
-    token_debug(tokens[i]);
+    for (int i = 0; i < token_count; i++) {
+      token_debug(tokens[i]);
+    }
   }
-
   return 0;
 }
+#else 
+int main(){
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
+#endif
